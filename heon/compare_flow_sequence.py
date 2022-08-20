12fa788f5f1f9287884faf33f0fa4ebae881df5c
12345120 <hehe9343@gmail.com>
2022-08-19 20:00:49 -0400

    Improved compare_flow_sequence.py
    * Added comparison between client input sequence and desired sequence

--- heon/compare_flow_sequence.py
+++ heon/compare_flow_sequence.py
@@ -14,6 +14,13 @@ def compare_lists(list1, list2):
 
 
 def compare_flow_sequence(args):
+    # Get client-side flow sequence input
+    # By default, input flow sequence for the client is equal to the desired flow sequence
+    f = open(args.client_seq_input, "r")
+    flow_input_lines_client = f.readlines()
+    flow_input_client = [line.strip() for line in flow_input_lines_client]
+    f.close()
+    
     # Get client-side flow sequence output
     f = open(args.client_seq_output, "r")
     flow_sequence_lines_client = f.readlines()
@@ -33,10 +40,12 @@ def compare_flow_sequence(args):
     f.close()
 
     # Get comparison results
+    client_input_desired_match = compare_lists(flow_input_client, flow_sequence_desired)
     client_server_match = compare_lists(flow_sequence_client, flow_sequence_server)
     server_desired_match = compare_lists(flow_sequence_server, flow_sequence_desired)
     
     f = open(args.compare_result_file, "w")
+    f.write(f"Client Input & Desired (Expected) Match => {client_input_desired_match}\n")
     f.write(f"Client & Server Match => {client_server_match}\n")
     f.write(f"Server & Desired (Expected) Match => {server_desired_match}\n")
     f.close()
@@ -45,6 +54,8 @@ def compare_flow_sequence(args):
 if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--client-seq-input", type=str,
+                        required=False, default="input_flow_sequence.txt", help="Name of file that contains the input flow sequence on the client side.")
     parser.add_argument("-c", "--client-seq-output", type=str,
                         required=False, default="flow_sequence_client_side.txt", help="Name of file that contains the output flow sequence on the client side.")
     parser.add_argument("-s", "--server-seq-output", type=str,
