27d5d004e9f31febe536b70db7fe5380bf6f2fb9
12345120 <hehe9343@gmail.com>
2022-08-19 22:34:21 -0400

    Minor fixes in test script

--- heon/automate_full_test.sh
+++ heon/automate_full_test.sh
@@ -21,32 +21,10 @@ clean () {
 	echo "======================================"
 }
 
-# remove current qdisc on router, but keep the htb max link rate
-reset_qdisc () {
-
-	echo "======================================"
-	echo "Resetting qdisc for next test..."
-  
-	sudo tc qdisc del dev $IF parent 1:1
-
-	echo "The qdisc has been reset"
-	echo "======================================"
-}
-
-# set max link rate to 50 Mbps with htb qdisc; this will remain throughout all the tests
 setup () {
 	sudo tc qdisc add dev $IF root handle 1: htb default 1
 
 	sudo tc class add dev $IF parent 1: classid 1:1 htb rate 50Mbit
-}
-
-# pause to allow user to run patient/surgeon scripts
-run () {
-	echo "Please run client/server programs now [$1]"
-
-	read -p "Once ready, press any key to continue... " -n1 -s
-
-	echo ""
 }
 
 run () {
